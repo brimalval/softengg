cf1bb3ccb8b290772af2e22cba1b7e244412be09
Brian Valencia <qbmsvalencia@tip.edu.ph>
2021-07-01 03:35:30 +0800

    Added snapshot styling to columns & converted col/task to PureComponents

--- src/client/src/components/Home/Home.tsx
+++ src/client/src/components/Home/Home.tsx
@@ -2,11 +2,28 @@ import React, { useState } from "react";
 import { tasks } from "../../utils/consts";
 import { DragDropContext, DropResult } from "react-beautiful-dnd";
 import { TaskColumn } from "./";
-import { Grid } from "@material-ui/core";
+import { AppBar, Grid, Tabs, Tab, Typography } from "@material-ui/core";
 
+interface ColumnListProps {
+    tasks: typeof tasks;
+}
+// Converted to a pure component to prevent unnecessary re-drawing
+class ColumnList extends React.PureComponent<ColumnListProps> {
+    render() {
+        const { tasks } = this.props;
+        return Object.entries(tasks.columns).map(([id, column]) => (
+            <Grid item xs={6} sm={4} md={3} key={`col${id}`}>
+                <TaskColumn
+                    title={column.title}
+                    id={column.id}
+                    tasks={column.tasks}
+                />
+            </Grid>
+        ));
+    }
+}
 
 const Home = () => {
-    const [currTasks, setTasks] = useState(tasks);
     const handleDragEnd = (result: DropResult) => {
         const destination = result.destination?.droppableId;
         const source = result.source.droppableId;
@@ -50,6 +67,7 @@ const Home = () => {
             tasks: newSourceTasks,
         };
 
+        console.log(currTasks);
         setTasks({
             ...currTasks,
             columns: {
@@ -59,18 +77,39 @@ const Home = () => {
         });
     };
 
+    const handleTabs = (event: React.ChangeEvent<{}>, newValue: string) => {
+        setTab(newValue);
+    };
+
+    const [currTasks, setTasks] = useState(tasks);
+    const [tab, setTab] = useState("main");
+
     return (
         <DragDropContext onDragEnd={handleDragEnd}>
-            <Grid container spacing={3} justify="center">
-                {Object.entries(currTasks.columns).map(([id, column]) => (
-                    <Grid item xs={6} sm={4} md={3} key={`col${id}`}>
-                        <TaskColumn
-                            title={column.title}
-                            id={column.id}
-                            tasks={column.tasks}
-                        />
-                    </Grid>
-                ))}
+            <AppBar position="relative" elevation={0}>
+                <Tabs
+                    onChange={handleTabs}
+                    value={tab}
+                    aria-label="tab selector"
+                >
+                    <Tab value="main" label="Tab One" />
+                    <Tab value="other" label="Tab Two" />
+                    <Tab value="third" label="Tab Three" />
+                </Tabs>
+            </AppBar>
+            <Grid
+                container
+                spacing={3}
+                justify="center"
+                style={{ padding: "5px" }}
+            >
+                {tab === "main" && <ColumnList tasks={currTasks} />}
+                {tab === "other" && (
+                    <Typography variant="h6">Second Page</Typography>
+                )}
+                {tab === "third" && (
+                    <Typography variant="h6">Third Page</Typography>
+                )}
             </Grid>
         </DragDropContext>
     );
